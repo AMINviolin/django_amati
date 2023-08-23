6473e3a3456a28fc1367a54cd4c6961788e466f4
AMINviolin <amin.djankiller@gmail.com>
2023-08-23 15:00:08 -0700

    nothing

--- root/models.py
+++ root/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class service(models.Model):
+    title = models.CharField(max_length=100)
+    created_date = models.DateTimeField()
+    content = 
--- root/views.py
+++ root/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,"root/index.html")
+    return render(request,"root/index.html",context = {'name': 'admin'})
 
 def about(request):
-    return render(request,"root/about.html")
+    return render(request,"root/about.html",context = {'make': '1998'})
 
 def contact(request):
-    return render(request,"root/contact.html")
+    return render(request,"root/contact.html",context = {'number': '45346'})
