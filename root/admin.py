ccdc40888f6ca6547f7aea986daa278306e851b6
AMINviolin <amin.djankiller@gmail.com>
2023-08-24 12:47:22 -0700

    no caption

--- root/admin.py
+++ root/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from models import service
+from .models import service
 
 admin.site.register(service)
--- root/models.py
+++ root/models.py
@@ -3,4 +3,6 @@ from django.db import models
 class service(models.Model):
     title = models.CharField(max_length=100)
     created_date = models.DateTimeField()
-    content = 
+
+    def __str__(self) :
+        return self.title
