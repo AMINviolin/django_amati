69d9009a34858ecf8b307997d020a933cf9f09e9
AMINviolin <amin.djankiller@gmail.com>
2023-08-25 15:49:56 -0700

    yaa

--- root/views.py
+++ root/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import service
+# from .models import service
 
 
 def home(request):
-    return render(request,"root/index.html",context = {'name': 'admin'})
+    return render(request,"root/index.html")
 
 def about(request):
-    return render(request,"root/about.html",context = {'make': '1998'})
+    return render(request,"root/about.html")
 
 def contact(request):
-    return render(request,"root/contact.html",context = {'number': '45346'})
+    return render(request,"root/contact.html")
