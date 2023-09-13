b30deb52ff41d700cc7e8ed59adccf0b6884478b
AMINviolin <amin.djankiller@gmail.com>
2023-09-13 08:20:49 -0700

    ,

--- root/urls.py
+++ root/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("",home,name='home'),
     path("about/",about,name='about'),
     path("contact/",contact,name='contact'),
-    path("trainers/",trainers,name='trainers'),
+    path("trainers/",trainers,name='trainers')
 ]
