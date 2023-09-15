de898624eec217058dc389f863697062d16988b6
AMINviolin <amin.djankiller@gmail.com>
2023-09-15 06:01:05 -0700

    pushing

--- root/views.py
+++ root/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import *
+from course.models import Courses,Trainer
 
 
 def home(request):
     services = Services.objects.filter(status=True)[:3]
-    context = {'service':services}
+    last_three_course = Courses.objects.filter(status=True)[:3]
+    last_three_trainer = Trainer.objects.filter(status=True)[:3]
+    context = {'service':services,
+               'course':last_three_course,
+               'trainer':last_three_trainer,
+               
+               }
     return render(request,"root/index.html",context=context)
 # _&_&_&_&_&_&_&_&_&_&_&_&_&_&_&_&
 
