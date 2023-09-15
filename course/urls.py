ac743d9d24ce47624aa328718737258afda61f57
AMINviolin <amin.djankiller@gmail.com>
2023-09-15 13:28:13 -0700

    fvjhghgujh

--- course/urls.py
+++ course/urls.py
@@ -6,5 +6,6 @@ app_name = 'course'
 urlpatterns = [
     path('',Maincourse,name='courses'),
     path('category/<str:cat>',Maincourse,name='course_cat'),
-    path('teacher/<str:teacher>',Maincourse,name='course_teacher')
+    path('teacher/<str:teacher>',Maincourse,name='course_teacher'),
+    path('search/',Maincourse,name='course_search'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
-from .models import *
+from .models import Courses
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 def Maincourse(request,cat=None,teacher=None):
     if cat:
         courses = Courses.objects.filter(category__name=cat)
     elif teacher:
         courses = Courses.objects.filter(teacher__info__username = teacher)
+    elif request.GET.get('search'):
+        courses = Courses.objects.filter(content__contains=request.GET.get('search'))
     else:
         courses = Courses.objects.filter(status = True)
+
+
+    courses = Paginator(courses,3)
+    first_page = 1
+    last_page = courses.num_pages
+    try:
+        page_number = request.GET.get('page')
+        courses = courses.get_page(page_number)
+
+    except EmptyPage:
+        courses = courses.get_page(1)
+
+    except PageNotAnInteger:
+        courses = courses.get_page(1)
+
+
+
     context = {
-        'maincourse': courses
+        'maincourse': courses,
+        'first_page':first_page,
+        'last_page':last_page,
     }
     return render(request,"courses/courses.html",context=context)
