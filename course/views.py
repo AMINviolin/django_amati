b481ec1de4664dd07092cd4ac95707fd70102769
AMINviolin <amin.djankiller@gmail.com>
2023-09-16 09:38:42 -0700

    course

--- course/views.py
+++ course/views.py
@@ -36,13 +36,12 @@ def Maincourse(request,cat=None,teacher=None):
     return render(request,"courses/courses.html",context=context)
 
 def course_detail(request,id):
-    try:
+    # try:
         course = Courses.objects.get(id=id)
         id_list = []
         course = Courses.objects.filter(status = True)
-        for cr in Courses :
+        for cr in course :
             id_list.append(cr.id)
-
         
         if id_list[0] == id:
             next_course = Courses.objects.get(id=id_list[1])
@@ -53,18 +52,18 @@ def course_detail(request,id):
             previous_course = Courses.objects.get(id=id_list[-2])
 
         else:
-            next_course = Courses.objects.get(id=id_list[id_list.index(id),+1])
+            next_course = Courses.objects.get(id=id_list[id_list.index(id)+1])
             previous_course = Courses.objects.get(id=id_list[id_list.index(id)-1])
 
 
 
-        course.counted_views +=1
+        course.counted_views += 1
         course.save()
         context = {
             'course': course,
             'next_course': next_course,
             'previous_course': previous_course,
         }
-        return render(request,"courses/coursedetails.html",context=context)
-    except:
-        return render(request,"courses/404.html")
+        return render(request,"courses/course-details.html",context=context)
+    # except:
+    #     return render(request,'courses/404.html')
